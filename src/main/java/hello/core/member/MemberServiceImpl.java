0f2638fe35ec2c3cfc74c57d7c7cecf999b300b7
ccminjun <mi478@naver.com>
2022-04-25 17:32:25 +0900

    2022-04-25 스프링 핵심 원리- AppConfig 리펙토링 까지 완료

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,11 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService{
 
     // 뒤에 안써주면 NullPointException이 터짐
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
