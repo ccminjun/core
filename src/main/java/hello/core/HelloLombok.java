eed395cf69cbfc83e91075d4e54f371465f19cbf
ccminjun <mi478@naver.com>
2022-05-03 17:58:17 +0900

    2022-05-03 의존관계 자동 주입

---
+++ src/main/java/hello/core/HelloLombok.java
@@ -0,0 +1,25 @@
+package hello.core;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Arrays;
+
+@Getter
+@Setter
+@ToString
+public class HelloLombok {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("asdasd");
+
+        System.out.println("helloLombok = " + helloLombok);
+        //String name = helloLombok.getName();
+        //System.out.println("name = " + name);
+    }
+}
